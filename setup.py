e708b35a1decbbf7ebf3beed3e5403062072a46f
Flo <171354410+kainow-code@users.noreply.github.com>
2024-08-27 20:23:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="wizzycord",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
-        # Fügen Sie hier eventuelle Abhängigkeiten hinzu
+        "py-cord>=2.0.0"
     ],
     author="kainow-code",
-    author_email="ihre.email@example.com",
-    description="Eine Python-Bibliothek namens WizzyCord",
+    author_email="ihre.email@example.com",  # Sie können diese E-Mail-Adresse nach Bedarf ändern
+    description="Eine Python-Bibliothek für einfaches Pycord-Bot-Cog-Management",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/kainow-code/wizzycord",
@@ -18,5 +18,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
