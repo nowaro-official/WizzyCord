ead314b936d11a4a71149a31b8d5ed07517abf7e
Flo <171354410+nowaro-official@users.noreply.github.com>
2025-04-27 23:56:32 +0200

    Update __init__.py

--- wizzycord/__init__.py
+++ wizzycord/__init__.py
@@ -2,7 +2,7 @@
 wizzycord - Utilities für die Verwaltung von Benutzerlisten mit Discord-Integration
 """
 
-from .user_list import UserList
+from .guard_list import GuardList
 from .guard_check import GuardCheck
 
-__all__ = ["UserList", "GuardCheck"]
+__all__ = ["GuardList", "GuardCheck"]
