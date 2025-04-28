d21f22ee5bd7cbeb588ef117a72f47b18621ce2d
Flo <171354410+nowaro-official@users.noreply.github.com>
2025-04-28 02:12:59 +0200

    Update __init__.py

--- wizzycord/__init__.py
+++ wizzycord/__init__.py
@@ -4,5 +4,26 @@ wizzycord - Utilities für die Verwaltung von Benutzerlisten mit Discord-Integra
 
 from .guard_list import GuardList
 from .guard_check import GuardCheck
+from .wizzycolor import (
+    init, 
+    display_bot_interface, 
+    colored_text, 
+    log_info, 
+    log_success, 
+    log_warning, 
+    log_error,
+    Colors
+)
 
-__all__ = ["GuardList", "GuardCheck"]
+__all__ = [
+    "GuardList", 
+    "GuardCheck", 
+    "init", 
+    "display_bot_interface", 
+    "colored_text", 
+    "log_info", 
+    "log_success", 
+    "log_warning", 
+    "log_error",
+    "Colors"
+]
